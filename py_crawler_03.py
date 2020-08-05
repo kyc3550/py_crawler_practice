2461373ee80f935538f6353e17c64b77ff3d4cb6
kyc3550 <rmawkxkq0909@naver.com>
2020-08-05 17:31:12 +0900

    finance o,c,h,l

--- py_crawler_03.py
+++ py_crawler_03.py
@@ -13,7 +13,29 @@ def get_candle_chart_data(company_code):
     blind = td_first.find("span", {"class":"blind"})
     #close 종가(전일)
     close = blind.text
-    return close
 
-candle_chart_data = get_candle_chart_data("000660")
-print(candle_chart_data)
+    #high 고가
+    table = bs_obj.find("table",{"class":"no_info"})
+    trs=table.findAll("tr")
+    first_tr = trs[0]
+    first_tr_tds = first_tr.findAll("td")
+    first_tr_tds_second_td = first_tr_tds[1]
+    high = first_tr_tds_second_td.find("span",{"class":"blind"}).text
+
+    #open 시가
+    second_tr = trs[1]
+    second_tr_td = second_tr.find("td",{"class":"first"})
+    blind_open = second_tr_td.find("span",{"class":"blind"})
+    open = blind_open.text
+    # low 저가
+    second_tr_tds = second_tr.findAll("td")
+    second_tr_sec_td = second_tr_tds[1]
+    blind_low = second_tr_sec_td.find("span",{"class":"blind"})
+    low = blind_low.text
+    return {"close":close,"high":high,"open":open,"low":low}
+
+company_codes = ["000660","035420","034220"]
+for item in company_codes:
+    candle_chart_data = get_candle_chart_data(item)
+    print(candle_chart_data)
+
